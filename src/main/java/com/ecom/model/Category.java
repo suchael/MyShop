e332333bf5472d79dfd73da91316d581f915164a
suchael <suchael8@gmail.com>
2022-06-05 12:00:00 +0100

    Add domain models

---
+++ src/main/java/com/ecom/model/Category.java
@@ -0,0 +1,25 @@
+package com.ecom.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.Builder;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+public class Category {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private String imageName;
+}
